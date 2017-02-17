16e87b8b2ad423d602f4caece6d4a85f9d8593f1
vincent lam <vincentlam060@gmail.com>
2017-02-16 21:52:40 -0800

    added bullet queue images and fixed z position of menu bar/bullet queue

--- MathOfTheDead/GameScene.swift
+++ MathOfTheDead/GameScene.swift
@@ -61,8 +61,8 @@ class GameScene: SKScene {
         
         addingGuns()
         addingGameOverLine()
-        addingTopMenuBar()
-        addBulletQueue()
+        addingTopMenuBar(zvalue: 2.0)
+        addBulletQueue(zvalue: 2.0)
         
         walkFrames = [SKTexture]()
         
@@ -80,7 +80,7 @@ class GameScene: SKScene {
         ))
     }
     
-    func addingTopMenuBar(){
+    func addingTopMenuBar(zvalue : Double){
         let menuWidth = frame.width/3
         let menuHeight = frame.height/16
         
@@ -94,19 +94,24 @@ class GameScene: SKScene {
         pauseButtonBox.fillColor = UIColor.red
         pauseButtonBox.strokeColor = UIColor.black
         pauseButtonBox.lineWidth = frame.size.width * 0.01
+        pauseButtonBox.zPosition = CGFloat(zvalue);
         addChild(pauseButtonBox)
         
         
+        pauseButton = SKLabelNode(fontNamed: "Arial")
+        pauseButton.text = "PAUSE"
+        pauseButton.fontSize = 20
         
-        //let scalingFactor = min(menuWidth/menuHeight)
+        // Determine the font scaling factor that should let the label text fit in the given rectangle.
+        let scalingFactor = min(pauseButtonBox.frame.width / pauseButton.frame.width, pauseButtonBox.frame.height / pauseButton.frame.height)
         
         // Change the fontSize.
-        //pauseButton.fontSize *= scalingFactor
+        pauseButton.fontSize *= scalingFactor
         
         // Optionally move the SKLabelNode to the center of the rectangle.
-        //pauseButton.position = CGPoint(x: xPos, y: yPos)
-        
-        
+        pauseButton.position = CGPoint(x: pauseButtonBox.frame.midX, y: pauseButtonBox.frame.midY - pauseButton.frame.height / 2.0)
+        pauseButton.zPosition = CGFloat(zvalue);
+        addChild(pauseButton)
         
         xPos = xPos + menuWidth
         yPos = frame.maxY - menuHeight
@@ -118,6 +123,7 @@ class GameScene: SKScene {
         waveNumberBox.fillColor = UIColor.red
         waveNumberBox.strokeColor = UIColor.black
         waveNumberBox.lineWidth = frame.size.width * 0.01
+        waveNumberBox.zPosition = CGFloat(zvalue);
         addChild(waveNumberBox)
         
         
@@ -131,11 +137,12 @@ class GameScene: SKScene {
         scoreDisplayBox.fillColor = UIColor.red
         scoreDisplayBox.strokeColor = UIColor.black
         scoreDisplayBox.lineWidth = frame.size.width * 0.01
+        scoreDisplayBox.zPosition = CGFloat(zvalue);
         addChild(scoreDisplayBox)
         
     }
     
-    func addBulletQueue(){
+    func addBulletQueue(zvalue: Double){
         let queueWidth = frame.width * 0.1875
         let queueHeight = frame.height/16
         
@@ -150,7 +157,16 @@ class GameScene: SKScene {
         bullQueue0Box.fillColor = UIColor.gray
         bullQueue0Box.strokeColor = UIColor.black
         bullQueue0Box.lineWidth = frame.size.width * 0.01
+        bullQueue0Box.zPosition = CGFloat(zvalue);
         addChild(bullQueue0Box)
+        
+        var bullet0 = SKSpriteNode(imageNamed: "bullet");
+        var aspectRatio = bullet0.size.width/bullet0.size.height
+        bullet0.size = CGSize(width: bullQueue0Box.frame.width, height: bullQueue0Box.frame.width/aspectRatio)
+        
+        bullet0.position = CGPoint(x: bullQueue0Box.frame.midX, y: bullQueue0Box.frame.midY)
+        bullet0.zPosition = CGFloat(zvalue);
+        addChild(bullet0)
         
         
         xPos = frame.minX + queueWidth
@@ -163,8 +179,16 @@ class GameScene: SKScene {
         bullQueue1Box.fillColor = UIColor.gray
         bullQueue1Box.strokeColor = UIColor.black
         bullQueue1Box.lineWidth = frame.size.width * 0.01
+        bullQueue1Box.zPosition = CGFloat(zvalue);
         addChild(bullQueue1Box)
         
+        var bullet1 = SKSpriteNode(imageNamed: "bullet");
+        aspectRatio = bullet1.size.width/bullet1.size.height
+        bullet1.size = CGSize(width: bullQueue1Box.frame.width, height: bullQueue0Box.frame.width/aspectRatio)
+        
+        bullet1.position = CGPoint(x: bullQueue1Box.frame.midX, y: bullQueue1Box.frame.midY)
+        bullet1.zPosition = CGFloat(zvalue);
+        addChild(bullet1)
         
         xPos = xPos + queueWidth
         bullQueue2Box = SKShapeNode()
@@ -175,7 +199,16 @@ class GameScene: SKScene {
         bullQueue2Box.fillColor = UIColor.gray
         bullQueue2Box.strokeColor = UIColor.black
         bullQueue2Box.lineWidth = frame.size.width * 0.01
+        bullQueue2Box.zPosition = CGFloat(zvalue);
         addChild(bullQueue2Box)
+        
+        var bullet2 = SKSpriteNode(imageNamed: "bullet");
+        aspectRatio = bullet2.size.width/bullet2.size.height
+        bullet2.size = CGSize(width: bullQueue2Box.frame.width, height: bullQueue2Box.frame.width/aspectRatio)
+        
+        bullet2.position = CGPoint(x: bullQueue2Box.frame.midX, y: bullQueue2Box.frame.midY)
+        bullet2.zPosition = CGFloat(zvalue);
+        addChild(bullet2)
         
         
         xPos = xPos + queueWidth
@@ -187,7 +220,16 @@ class GameScene: SKScene {
         bullQueue3Box.fillColor = UIColor.gray
         bullQueue3Box.strokeColor = UIColor.black
         bullQueue3Box.lineWidth = frame.size.width * 0.01
+        bullQueue3Box.zPosition = CGFloat(zvalue);
         addChild(bullQueue3Box)
+        
+        var bullet3 = SKSpriteNode(imageNamed: "bullet");
+        aspectRatio = bullet3.size.width/bullet3.size.height
+        bullet3.size = CGSize(width: bullQueue3Box.frame.width, height: bullQueue3Box.frame.width/aspectRatio)
+        
+        bullet3.position = CGPoint(x: bullQueue3Box.frame.midX, y: bullQueue3Box.frame.midY)
+        bullet3.zPosition = CGFloat(zvalue);
+        addChild(bullet3)
         
         
         xPos = xPos + queueWidth
@@ -200,7 +242,16 @@ class GameScene: SKScene {
         bullQueue4Box.fillColor = UIColor.gray
         bullQueue4Box.strokeColor = UIColor.black
         bullQueue4Box.lineWidth = frame.size.width * 0.01
+        bullQueue4Box.zPosition = CGFloat(zvalue);
         addChild(bullQueue4Box)
+        
+        var bullet4 = SKSpriteNode(imageNamed: "bullet");
+        aspectRatio = bullet4.size.width/bullet4.size.height
+        bullet4.size = CGSize(width: bullQueue4Box.frame.width, height: bullQueue4Box.frame.width/aspectRatio)
+        
+        bullet4.position = CGPoint(x: bullQueue4Box.frame.midX, y: bullQueue4Box.frame.midY)
+        bullet4.zPosition = CGFloat(zvalue);
+        addChild(bullet4)
         
     }
     
@@ -338,7 +389,7 @@ class GameScene: SKScene {
             }
         }
     }
-        func addZombie() {
+    func addZombie() {
         // NOTE: GRID SYSTEM USES 0,0 AS THE CENTER OF THE SCREEN!!!
         
         // Create sprite
@@ -358,7 +409,7 @@ class GameScene: SKScene {
         // Position the monster slightly off-screen along the right edge,
         // and along a random position along the Y axis as calculated above
         zombie.position = CGPoint(x: actualX, y: frame.maxY + zombie.size.height)
-        
+        zombie.zPosition = CGFloat(1.0);
         // Add the monster to the scene
         addChild(zombie)
         
