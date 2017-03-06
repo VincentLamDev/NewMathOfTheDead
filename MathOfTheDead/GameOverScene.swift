d0144100934cf51d0abd4c50387d8475e255d95f
Vincent Lam <vincentlam060@gmail>
2017-03-06 14:46:39 -0800

    created home screen interface

--- MathOfTheDead/GameOverScene.swift
+++ MathOfTheDead/GameOverScene.swift
@@ -11,35 +11,31 @@ import SpriteKit
 
 class GameOverScene: SKScene {
     
-    init(size: CGSize, won:Bool) {
+    
+    var restartBox: SKShapeNode!
+    var restartLabel: SKLabelNode!
+    var mainMenuBox: SKShapeNode!
+    var mainMenuLabel: SKLabelNode!
+    
+    
+    init(size: CGSize, won:Bool, score: Int, wave: Int) {
         
         super.init(size: size)
         
-        
+        //background grass tile map
         let bgTexture = SKTexture(imageNamed: "dark-grass")
         let bgDefinition = SKTileDefinition(texture: bgTexture, size: bgTexture.size())
         let bgGroup = SKTileGroup(tileDefinition: bgDefinition)
         let tileSet = SKTileSet(tileGroups: [bgGroup])
-        //let bgNode = SKTileMapNode(tileSet: tileSet, columns: 4, rows: 8, tileSize: bgTexture.size())
-        
         let bgNode = SKTileMapNode(tileSet: tileSet, columns: 4, rows: 8, tileSize: bgTexture.size(), fillWith: bgGroup)
-        
         bgNode.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
         bgNode.setScale(1)
-        
         self.addChild(bgNode)
         
         
         
-        
-        
-        // 1
-        backgroundColor = SKColor.white
-        
-        // 2
+        //Game over label
         let message = "GAME OVER"
-        
-        // 3
         let label = SKLabelNode(fontNamed: "Chalkduster")
         label.text = message
         label.fontSize = 80
@@ -47,74 +43,77 @@ class GameOverScene: SKScene {
         label.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - label.frame.height)
         addChild(label)
         
-        let restartBox = SKShapeNode()
         
+        
+        //Displaying player score
+        let scorelabel = SKLabelNode(fontNamed: "Arial")
+        scorelabel.text = "Score: " + String(score)
+        scorelabel.fontSize = 20
+        var scoreLabelScaleFactor = min((self.frame.width * 0.7) / scorelabel.frame.width, (self.frame.height * 0.15) / scorelabel.frame.height)
+        scorelabel.fontSize *= scoreLabelScaleFactor
+        scorelabel.position = CGPoint(x: self.frame.midX, y: self.frame.maxY * 0.75)
+        addChild(scorelabel)
+
+        //Displaying player kills
+        let killslabel = SKLabelNode(fontNamed: "Arial")
+        killslabel.text = "Kills: " + String(wave)
+        killslabel.fontSize = 20
+        var killLabelScaleFactor = min((self.frame.width * 0.55) / killslabel.frame.width, (self.frame.height * 0.15) / killslabel.frame.height)
+        killslabel.fontSize *= killLabelScaleFactor
+        killslabel.position = CGPoint(x: self.frame.midX, y: self.frame.maxY * 0.6)
+        addChild(killslabel)
+
+        
+        
+        //Restart game button
+        restartBox = SKShapeNode()
         let btnWidth = self.frame.width/2
         let btnHeight = self.frame.height/10
         var xPos = self.frame.minX + self.frame.width/4
-        var yPos = self.frame.midY
+        var yPos = self.frame.maxY * 0.4
         restartBox.path = UIBezierPath(roundedRect: CGRect(x: xPos,
                                                               y: yPos,
                                                               width: btnWidth,
                                                               height: btnHeight), cornerRadius: 0).cgPath
-        
         restartBox.fillColor = UIColor.red
         restartBox.strokeColor = UIColor.black
         restartBox.lineWidth = frame.size.width * 0.01
         addChild(restartBox)
         
-        let mainMenuBox = SKShapeNode()
-        yPos = self.frame.midY - self.frame.height * 0.25
+        
+        //Restart button label
+        restartLabel = SKLabelNode(fontNamed: "Arial")
+        restartLabel.text = "Restart"
+        restartLabel.fontSize = 20
+        var scalingFactor = min(restartBox.frame.width / restartLabel.frame.width, restartBox.frame.height / restartLabel.frame.height)
+        restartLabel.fontSize *= (scalingFactor * 0.5)
+        restartLabel.position = CGPoint(x: restartBox.frame.midX, y: restartBox.frame.midY - restartLabel.frame.height / 2.0)
+        addChild(restartLabel)
+        
+        
+        //Return to main menu button
+        mainMenuBox = SKShapeNode()
+        yPos = self.frame.maxY * 0.2
         mainMenuBox.path = UIBezierPath(roundedRect: CGRect(x: xPos,
                                                            y: yPos,
                                                            width: btnWidth,
                                                            height: btnHeight), cornerRadius: 0).cgPath
-
         mainMenuBox.fillColor = UIColor.red
         mainMenuBox.strokeColor = UIColor.black
         mainMenuBox.lineWidth = frame.size.width * 0.01
         addChild(mainMenuBox)
+        
+        
+        //Main menu button label
+        mainMenuLabel = SKLabelNode(fontNamed: "Arial")
+        mainMenuLabel.text = "Main Menu"
+        mainMenuLabel.fontSize = 20
+        scalingFactor = min(mainMenuBox.frame.width / mainMenuLabel.frame.width, mainMenuBox.frame.height / mainMenuLabel.frame.height)
+        mainMenuLabel.fontSize *= (scalingFactor * 0.7)
+        mainMenuLabel.position = CGPoint(x: mainMenuBox.frame.midX, y: mainMenuBox.frame.midY - mainMenuLabel.frame.height / 2.0)
+        addChild(mainMenuLabel)
+        
 
-//        waveNumberBox.fillColor = UIColor.red
-//        waveNumberBox.strokeColor = UIColor.black
-//        waveNumberBox.lineWidth = frame.size.width * 0.01
-//        waveNumberBox.zPosition = CGFloat(zvalue);
-//        addChild(waveNumberBox)
-//        
-//        waveNumberLabel = SKLabelNode(fontNamed: "Arial")
-//        
-//        waveNumberLabel.text = "Kills: 0"
-//        waveNumberLabel.fontSize = 20
-//        
-//        // Determine the font scaling factor that should let the label text fit in the given rectangle.
-//        scalingFactor = min(waveNumberBox.frame.width / waveNumberLabel.frame.width, waveNumberBox.frame.height / pauseButton.frame.height)
-//        
-//        // Change the fontSize.
-//        waveNumberLabel.fontSize *= scalingFactor
-//        
-//        // Optionally move the SKLabelNode to the center of the rectangle.
-//        waveNumberLabel.position = CGPoint(x: waveNumberBox.frame.midX, y: waveNumberBox.frame.midY - waveNumberLabel.frame.height / 2.0)
-//        waveNumberLabel.zPosition = CGFloat(zvalue);
-//        addChild(waveNumberLabel)
-
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // 4
         run(SKAction.sequence([
             SKAction.wait(forDuration: 3.0),
             SKAction.run() {
---
+++ MathOfTheDead/HomeScene.swift
@@ -0,0 +1,156 @@
+//
+//  HomeScene.swift
+//  MathOfTheDead
+//
+//  Created by Vincent Lam on 2017-03-06.
+//  Copyright © 2017 NGU. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class HomeScene: SKScene {
+    
+    
+    var startBox: SKShapeNode!
+    var startLabel: SKLabelNode!
+    var leaderboardBox: SKShapeNode!
+    var leaderboardLabel: SKLabelNode!
+    var settingsBox: SKShapeNode!
+    var settingsLabel: SKLabelNode!
+    
+    
+    init(size: CGSize, won:Bool, score: Int, wave: Int) {
+        
+        super.init(size: size)
+        
+        //background grass tile map
+        let bgTexture = SKTexture(imageNamed: "dark-grass")
+        let bgDefinition = SKTileDefinition(texture: bgTexture, size: bgTexture.size())
+        let bgGroup = SKTileGroup(tileDefinition: bgDefinition)
+        let tileSet = SKTileSet(tileGroups: [bgGroup])
+        //let bgNode = SKTileMapNode(tileSet: tileSet, columns: 4, rows: 8, tileSize: bgTexture.size())
+        
+        let bgNode = SKTileMapNode(tileSet: tileSet, columns: 4, rows: 8, tileSize: bgTexture.size(), fillWith: bgGroup)
+        
+        bgNode.position = CGPoint(x: self.frame.size.width / 2, y: self.frame.size.height / 2)
+        bgNode.setScale(1)
+        self.addChild(bgNode)
+        
+        
+        
+        //Game over label
+        let label1 = SKLabelNode(fontNamed: "Chalkduster")
+        label1.text = "MATH OF"
+        label1.fontSize = 80
+        label1.fontColor = SKColor.red
+        var scalingFactor = min(self.frame.width * 0.7 / label1.frame.width, self.frame.height * 0.15 / label1.frame.height)
+        label1.fontSize *= scalingFactor
+        label1.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - label1.frame.height * 1.10)
+        addChild(label1)
+        
+        let label2 = SKLabelNode(fontNamed: "Chalkduster")
+        label2.text = "THE DEAD"
+        label2.fontSize = 80
+        label2.fontColor = SKColor.red
+        scalingFactor = min(self.frame.width * 0.7 / label2.frame.width, self.frame.height * 0.15 / label2.frame.height)
+        label2.fontSize *= scalingFactor
+        label2.position = CGPoint(x: self.frame.midX, y: self.frame.maxY - label1.frame.height * 2 )
+        addChild(label2)
+        
+        
+        
+        
+        
+        //Start game button
+        startBox = SKShapeNode()
+        let btnWidth = self.frame.width/2
+        let btnHeight = self.frame.height/10
+        var xPos = self.frame.minX + self.frame.width/4
+        var yPos = self.frame.maxY * 0.6
+        startBox.path = UIBezierPath(roundedRect: CGRect(x: xPos,
+                                                           y: yPos,
+                                                           width: btnWidth,
+                                                           height: btnHeight), cornerRadius: 0).cgPath
+        startBox.fillColor = UIColor.red
+        startBox.strokeColor = UIColor.black
+        startBox.lineWidth = frame.size.width * 0.01
+        addChild(startBox)
+        
+        
+        //Start button label
+        startLabel = SKLabelNode(fontNamed: "Arial")
+        startLabel.text = "Restart"
+        startLabel.fontSize = 20
+        scalingFactor = min(startBox.frame.width / startLabel.frame.width, startBox.frame.height / startLabel.frame.height)
+        startLabel.fontSize *= (scalingFactor * 0.4)
+        startLabel.position = CGPoint(x: startBox.frame.midX, y: startBox.frame.midY - startLabel.frame.height / 2.0)
+        addChild(startLabel)
+        
+        
+        //Leaderboard button
+        leaderboardBox = SKShapeNode()
+        yPos = self.frame.maxY * 0.4
+        leaderboardBox.path = UIBezierPath(roundedRect: CGRect(x: xPos,
+                                                            y: yPos,
+                                                            width: btnWidth,
+                                                            height: btnHeight), cornerRadius: 0).cgPath
+        leaderboardBox.fillColor = UIColor.red
+        leaderboardBox.strokeColor = UIColor.black
+        leaderboardBox.lineWidth = frame.size.width * 0.01
+        addChild(leaderboardBox)
+        
+        
+        //Leaderboard button label
+        leaderboardLabel = SKLabelNode(fontNamed: "Arial")
+        leaderboardLabel.text = "High Scores"
+        leaderboardLabel.fontSize = 20
+        scalingFactor = min(leaderboardBox.frame.width / leaderboardLabel.frame.width, leaderboardBox.frame.height / leaderboardLabel.frame.height)
+        leaderboardLabel.fontSize *= (scalingFactor * 0.7)
+        leaderboardLabel.position = CGPoint(x: leaderboardBox.frame.midX, y: leaderboardBox.frame.midY - leaderboardLabel.frame.height / 2.0)
+        addChild(leaderboardLabel)
+        
+        
+        //Setting button
+        settingsBox = SKShapeNode()
+        yPos = self.frame.maxY * 0.2
+        settingsBox.path = UIBezierPath(roundedRect: CGRect(x: xPos,
+                                                               y: yPos,
+                                                               width: btnWidth,
+                                                               height: btnHeight), cornerRadius: 0).cgPath
+        settingsBox.fillColor = UIColor.red
+        settingsBox.strokeColor = UIColor.black
+        settingsBox.lineWidth = frame.size.width * 0.01
+        addChild(settingsBox)
+        
+        
+        //Settings button label
+        settingsLabel = SKLabelNode(fontNamed: "Arial")
+        settingsLabel.text = "High Scores"
+        settingsLabel.fontSize = 20
+        scalingFactor = min(settingsBox.frame.width / settingsLabel.frame.width, settingsBox.frame.height / settingsLabel.frame.height)
+        settingsLabel.fontSize *= (scalingFactor * 0.7)
+        settingsLabel.position = CGPoint(x: leaderboardBox.frame.midX, y: settingsBox.frame.midY - settingsLabel.frame.height / 2.0)
+        addChild(settingsLabel)
+        
+        
+        
+        run(SKAction.sequence([
+            SKAction.wait(forDuration: 3.0),
+            SKAction.run() {
+                // 5
+                
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                let scene = GameScene(size: size)
+                scene.reloadInputViews()
+                self.view?.presentScene(scene, transition:reveal)
+            }
+            ]))
+        
+    }
+    
+    // 6
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
